b0be90bc806b1749e31ec106e81eaecf9bb7f85d
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-09-09 08:46:48 +0200

    Remove Old adafruit header fiels

--- src/graphics/Adafruit_GFX/Adafruit_GFX.h
+++ src/graphics/Adafruit_GFX/Adafruit_GFX.h
@@ -6,8 +6,6 @@
 
 #include "gfxfont.h"
 
-#include <Adafruit_I2CDevice.h>
-#include <Adafruit_SPIDevice.h>
 
 /// A generic graphics superclass that can handle all sorts of drawing. At a
 /// minimum you can subclass and provide drawPixel(). At a maximum you can do a
