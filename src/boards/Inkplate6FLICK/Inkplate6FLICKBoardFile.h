70f8b32926548d0eda6631cf00e88d80112986eb
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-10-29 10:17:22 +0100

    Add Inkplate6FLICK port

---
+++ src/boards/Inkplate6FLICK/Inkplate6FLICKBoardFile.h
@@ -0,0 +1,31 @@
+/**
+ **************************************************
+ *
+ * @file        Inkplate6BoardFile.h
+ * @brief       Wrrapper for the different Inkplate boards and
+ *              it's classes.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     Borna Biro for soldered.com
+ ***************************************************/
+
+// Header guard.
+#ifndef __INKPLATE6FLICK_BOARD_SELECT_H__
+#define __INKPLATE6FLICK_BOARD_SELECT_H__
+
+// Board select check.
+#ifdef ARDUINO_INKPLATE6FLICK
+
+// Include Inkplate6 board header file.
+#include "Inkplate6FLICKDriver.h"
+
+// Wrapper for different Inkplate boards.
+class InkplateBoardClass : public EPDDriver
+{
+  public:
+    InkplateBoardClass(){};
+};
+
+#endif
+#endif
