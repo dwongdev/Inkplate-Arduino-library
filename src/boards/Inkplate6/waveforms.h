a0f3b22c09af1e337cff56d5c8eadf8062f0272e
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-10-29 10:17:50 +0100

    Add Inkplate 6 COLOR

--- src/boards/Inkplate6/waveforms.h
+++ src/boards/Inkplate6/waveforms.h
@@ -9,12 +9,7 @@
      {0, 0, 0, 0, 1, 1, 2, 0, 0}, {0, 0, 0, 0, 0, 0, 2, 0, 0}};
 
 
-#ifndef E_INK_WIDTH
 #define E_INK_WIDTH 800
-#endif
-#ifndef E_INK_HEIGHT
 #define E_INK_HEIGHT 600
-#endif
-
 #endif
 #endif
